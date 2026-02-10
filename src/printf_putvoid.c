07e05625690700509e7af0e4cbc57c7aa954d9c2
jfox <johnmatthewfoxpro@gmail.com>
2026-02-10 19:18:22 +0100

    Updated 3 ft_print functions that were throwing errors when compiled outside of school 42. Specifically when calling negative numbers in condtions with unsigned ints. Putvoid, putbr_unsigned and putnbr_hex_fd are affected.

--- src/printf_putvoid.c
+++ src/printf_putvoid.c
@@ -6,7 +6,7 @@
 /*   By: jfox <jfox.42angouleme@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 12:24:17 by jfox              #+#    #+#             */
-/*   Updated: 2025/11/19 15:18:22 by jfox             ###   ########.fr       */
+/*   Updated: 2026/02/10 19:14:25 by jfox             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ static int	get_length(unsigned long n, int bs)
 	int	digit;
 
 	digit = 0;
-	if (n < 0)
-		n = n * -1;
 	if (n == 0)
 		return (1);
 	while (n > 0)
